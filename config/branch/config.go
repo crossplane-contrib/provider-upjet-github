f45d449ddfe980b633225ff26826c008d64c7df0
Atze de Vries <atze.wiebe.de.vries@coop.no>
2023-01-16 15:07:06 +0100

    fix linting

--- config/branch/config.go
+++ config/branch/config.go
@@ -2,6 +2,7 @@ package branch
 
 import "github.com/upbound/upjet/pkg/config"
 
+// Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("github_branch", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
