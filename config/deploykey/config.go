3a06a9141f088c8a093659e51d1e63c4bd313a05
tomasmota <tomasrebelomota@gmail.com>
2024-04-07 19:18:23 +0200

    fix comment for deploy key

--- config/deploykey/config.go
+++ config/deploykey/config.go
@@ -4,17 +4,12 @@ import (
 	"github.com/crossplane/upjet/pkg/config"
 )
 
-// Configure github_branch resource.
+// Configure github_repository_deploy_key resource.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("github_repository_deploy_key", func(r *config.Resource) {
-		// We need to override the default group that upjet generated for
-		// this resource, which would be "github"
 		r.Kind = "DeployKey"
 		r.ShortGroup = "repo"
-		// This resource need the repository in which branch would be created
-		// as an input. And by defining it as a reference to Repository
-		// object, we can build cross resource referencing. See
-		// repositoryRef in the example in the Testing section below.
+
 		r.References["repository"] = config.Reference{
 			Type: "Repository",
 		}
