5d4721ef9a3107eee6f1075527215680ed15432a
Yu <yu.yang@anz.com>
2025-01-14 16:38:35 +1100

    generate file
    Signed-off-by: Yu <yu.yang@anz.com>

--- apis/team/v1alpha1/zz_generated.conversion_hubs.go
+++ apis/team/v1alpha1/zz_generated.conversion_hubs.go
@@ -10,6 +10,9 @@ package v1alpha1
 func (tr *EmuGroupMapping) Hub() {}
 
 // Hub marks this type as a conversion hub.
+func (tr *Members) Hub() {}
+
+// Hub marks this type as a conversion hub.
 func (tr *Team) Hub() {}
 
 // Hub marks this type as a conversion hub.
