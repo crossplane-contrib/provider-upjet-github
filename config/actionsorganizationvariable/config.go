431b324c9043c71447bfe1e05e11a894b5b2acaf
shuo.zhao <shuo.zhao@anz.com>
2025-01-14 14:17:25 +1100

    changed naming convention, to make it comply terraform naming convention
    Signed-off-by: shuo.zhao <shuo.zhao@anz.com>

--- config/actionsorganizationvariable/config.go
+++ config/actionsorganizationvariable/config.go
@@ -6,6 +6,7 @@ import "github.com/crossplane/upjet/pkg/config"
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("github_actions_organization_variable", func(r *config.Resource) {
 
+		r.Kind = "ActionsOrganizationVariable"
 		r.ShortGroup = "actions"
 
 		r.References["repository"] = config.Reference{
